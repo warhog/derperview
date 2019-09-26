76b7e869ad50f39a4bd34bf050db0ede707c7f0d
warhog <547310+warhog@users.noreply.github.com>
2019-09-26 13:32:28 +0200

    test multi os build

--- ci-scripts/build_and_install.sh
+++ ci-scripts/build_and_install.sh
@@ -11,29 +11,19 @@ echo "create new build folder"
 mkdir build
 cd build
 
-echo "run cmake for out of source build"
-INSTALL_PREFIX=""
-if [[ ${EUID} -eq 0 ]]; then
-    echo "script is run as root, start installation to local system bin dir"
-else
-    echo "script was not run as root, install to local bin dir"
-    INSTALL_PREFIX=-DCMAKE_INSTALL_PREFIX=../
-fi
-
+echo "running cmake with out of source build setup"
 if [[ "${TRAVIS_OS_NAME}" == "windows" ]]; then
     # on travis windows builds ignore sh.exe
-    cmake ../ -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release ${INSTALL_PREFIX}
+    cmake ../ -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../
     if [[ $? -ne 0 ]]; then
         echo "cmake configuration failed"
-        cat CMakeFiles/CMakeOutput.log
         cat CMakeFiles/CMakeError.log
         exit 1
     fi
 else
-    cmake ../ -DCMAKE_BUILD_TYPE=Release ${INSTALL_PREFIX}
+    cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../
     if [[ $? -ne 0 ]]; then
         echo "cmake configuration failed"
-        cat CMakeFiles/CMakeOutput.log
         cat CMakeFiles/CMakeError.log
         exit 1
     fi
@@ -48,4 +38,8 @@ fi
 
 echo "installing built binaries"
 cmake --build . --target install
+if [[ $? -ne 0 ]]; then
+    echo "install failed"
+    exit 1
+fi
 
--- ci-scripts/package-windows.sh
+++ ci-scripts/package-windows.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd bin
-echo $(pwd)
 cp ../ffmpeg-shared-windows/bin/swresample-3.dll ./
 cp ../ffmpeg-shared-windows/bin/avcodec-58.dll ./
 cp ../ffmpeg-shared-windows/bin/avformat-58.dll ./
 cp ../ffmpeg-shared-windows/bin/avutil-56.dll ./
-echo $(ls -l)
 7z a derperview-win64.zip derperview.exe swresample-3.dll avcodec-58.dll avformat-58.dll avutil-56.dll
 
+rm *.dll
+rm *.exe
