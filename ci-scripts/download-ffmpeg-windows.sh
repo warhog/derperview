bd4ca8f3859abbcb8639ee1acaa949aa0da58bc7
warhog <547310+warhog@users.noreply.github.com>
2019-09-26 13:14:17 +0200

    test multi os build

--- ci-scripts/download-ffmpeg-windows.sh
+++ ci-scripts/download-ffmpeg-windows.sh
@@ -10,3 +10,5 @@ wget https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-${FFMPEG_VERSION}-win
 7z x -y ffmpeg-${FFMPEG_VERSION}-win64-shared.zip
 mv ffmpeg-${FFMPEG_VERSION}-win64-shared ffmpeg-shared-windows
 
+echo "pwd download: $(pwd)"
+
--- ci-scripts/package-windows.sh
+++ ci-scripts/package-windows.sh
@@ -2,7 +2,13 @@
 
 cd bin
 echo $(pwd)
-7z a derperview-win64.zip derperview ../ffmpeg-shared-windows/swresample-3.dll ../ffmpeg-shared-windows/avcodec-58.dll ../ffmpeg-shared-windows/avformat-58.dll ../ffmpeg-shared-windows/avutil-56.dll
-
-ls -l ..
+cp ..&ffmpeg-shared-windows/swresample-3.dll ./
+cp ..&ffmpeg-shared-windows/avcodec-58.dll ./
+cp ..&ffmpeg-shared-windows/avformat-58.dll ./
+cp ..&ffmpeg-shared-windows/avutil-56.dll ./
+cd ..
+echo $(pwd)
+echo $(ls -l)
+cp -r bin derperview
+7z a derperview-win64.zip derperview/
 
