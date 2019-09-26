d84d320f71b48a9f98ce45abcf0b0ad273921399
warhog <547310+warhog@users.noreply.github.com>
2019-09-26 13:09:49 +0200

    test multi os build

--- ci-scripts/package-windows.sh
+++ ci-scripts/package-windows.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd bin
+echo $(pwd)
 7z a derperview-win64.zip derperview ../ffmpeg-shared-windows/swresample-3.dll ../ffmpeg-shared-windows/avcodec-58.dll ../ffmpeg-shared-windows/avformat-58.dll ../ffmpeg-shared-windows/avutil-56.dll
+
+ls -l ..
 
