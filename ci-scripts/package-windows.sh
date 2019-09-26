ebc1eeca0e208c1c9436dbe09e2a322402e4f6e7
warhog <547310+warhog@users.noreply.github.com>
2019-09-26 13:17:48 +0200

    test multi os build

--- ci-scripts/package-windows.sh
+++ ci-scripts/package-windows.sh
@@ -2,13 +2,10 @@
 
 cd bin
 echo $(pwd)
-cp ..&ffmpeg-shared-windows/swresample-3.dll ./
-cp ..&ffmpeg-shared-windows/avcodec-58.dll ./
-cp ..&ffmpeg-shared-windows/avformat-58.dll ./
-cp ..&ffmpeg-shared-windows/avutil-56.dll ./
-cd ..
-echo $(pwd)
+cp ../ffmpeg-shared-windows/swresample-3.dll ./
+cp ../ffmpeg-shared-windows/avcodec-58.dll ./
+cp ../ffmpeg-shared-windows/avformat-58.dll ./
+cp ../ffmpeg-shared-windows/avutil-56.dll ./
 echo $(ls -l)
-cp -r bin derperview
-7z a derperview-win64.zip derperview/
+7z a derperview-win64.zip derperview.exe swresample-3.dll avcodec-58.dll avformat-58.dll avutil-56.dll
 
